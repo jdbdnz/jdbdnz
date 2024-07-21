84ab557f41b9d55be237a9428cc6eec089c28b2d
Josh Dean <jdbdean@gmail.com>
2024-07-21 12:10:41 -0700

    feat: gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
+const isDev = process.env.NODE_ENV === 'development'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: isDev },
 
   css: ['~/assets/css/main.css'],
   postcss: {
@@ -16,5 +18,15 @@ export default defineNuxtConfig({
     preset: "cloudflare-pages"
   },
 
-  modules: ["nitro-cloudflare-dev", "@nuxt/content", "@nuxt/image"]
+  modules: [
+    "nitro-cloudflare-dev",
+    "@nuxt/content",
+    "@nuxt/image",
+    '@zadigetvoltaire/nuxt-gtm',
+  ],
+
+  gtm: {
+    id: 'GTM-KX488PCF',
+    enabled: !isDev,
+  }
 })
